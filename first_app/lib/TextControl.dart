b8455391ddefb17fef15bdf46e1a836e605c2e6f
Tolu Okeowo <TOkeowo@its.jnj.com>
2019-06-24 10:09:40 -0400

    Revert "yes"
    This reverts commit 1c339289c030572c4b5c4f8b19f5e8d525a3323a.

--- first_app/lib/TextControl.dart
+++ first_app/lib/TextControl.dart
@@ -13,7 +13,7 @@ class Textcontrol extends StatefulWidget{
 } 
 
 class _Textcontrol extends State<Textcontrol>{
-String _words = "I have been";
+String _words = "I have been here for 1 week now";
 
 @override
   Widget build(BuildContext context) {
